6d240089eae3766b9fc369b51109b0acd07229bf
pat <dimuziop@gmail.com>
2022-02-21 15:45:47 +0100

    feature: adding a service file

--- posts/index.js
+++ posts/index.js
@@ -5,4 +5,7 @@ const app = require('./src');
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => console.log(`Server is live at localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Version 21`)
+  console.log(`Server is live at localhost:${PORT}`)
+});
