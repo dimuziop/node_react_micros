805a4f577680976e0e92b04068fe01d4d6529fd2
pat <dimuziop@gmail.com>
2022-02-22 08:19:12 +0100

    refactor: applying services ostnames to event bus

--- event-bus/src/index.js
+++ event-bus/src/index.js
@@ -8,8 +8,20 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const POSTS_SERVICE_HOSTNAME = process.env.POSTS_SERVICE_HOSTNAME || "localhost";
-const POSTS_SERVICE_PORT = process.env.POSTS_SERVICE_PORT || "4005";
+const POSTS_SERVICE_PORT = process.env.POSTS_SERVICE_PORT || "4000";
 const BASE_POSTS_URL = `http://${POSTS_SERVICE_HOSTNAME}:${POSTS_SERVICE_PORT}`;
+
+const COMMENTS_SERVICE_HOSTNAME = process.env.COMMENTS_SERVICE_HOSTNAME || "localhost";
+const COMMENTS_SERVICE_PORT = process.env.COMMENTS_SERVICE_PORT || "4000";
+const BASE_COMMENTS_URL = `http://${COMMENTS_SERVICE_HOSTNAME}:${COMMENTS_SERVICE_PORT}`;
+
+const MODERATION_SERVICE_HOSTNAME = process.env.MODERATION_SERVICE_HOSTNAME || "localhost";
+const MODERATION_SERVICE_PORT = process.env.MODERATION_SERVICE_PORT || "4000";
+const BASE_MODERATION_URL = `http://${MODERATION_SERVICE_HOSTNAME}:${MODERATION_SERVICE_PORT}`;
+
+const QUERY_SERVICE_HOSTNAME = process.env.QUERY_SERVICE_HOSTNAME || "localhost";
+const QUERY_SERVICE_PORT = process.env.QUERY_SERVICE_PORT || "4000";
+const BASE_QUERY_URL = `http://${QUERY_SERVICE_HOSTNAME}:${QUERY_SERVICE_PORT}`;
 
 
 const events = [];
@@ -23,17 +35,16 @@ app.post("/events", (req, res) => {
   axios.post(`${BASE_POSTS_URL}/events`, event).catch((err) => {
     console.log(err.message);
   });
-  // axios.post('http://localhost:4001/events', event).catch((err) => {
-  //   console.log(err.message);
-  // });
-  // axios.post('http://localhost:4002/events', event).catch((err) => {
-  //   console.log(err.message);
-  // });
-  // res.send({ status: 'OK' });
+  axios.post(`${BASE_COMMENTS_URL}/events`, event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post(`${BASE_QUERY_URL}/events`, event).catch((err) => {
+    console.log(err.message);
+  });
 
-  // axios.post('http://localhost:4003/events', event).catch((err) => {
-  //   console.log(err.message);
-  // });
+  axios.post(`${BASE_MODERATION_URL}/events`, event).catch((err) => {
+    console.log(err.message);
+  });
   res.send({ status: 'OK' });
 
 });
