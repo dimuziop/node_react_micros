b647c480b4a03e0ed4f5fa024b86c7f50064ec1b
pat <dimuziop@gmail.com>
2022-02-22 07:20:37 +0100

    refactor: applying new event hostnames on posts service

--- posts/src/index.js
+++ posts/src/index.js
@@ -8,6 +8,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const EVENT_BUS_SERVICE_HOSTNAME = process.env.EVENT_BUS_SERVICE_HOSTNAME || "localhost";
+const EVENT_BUS_SERVICE_PORT = process.env.EVENT_BUS_SERVICE_PORT || "4005";
+const BASE_EVENT_BUS_URL = `http://${EVENT_BUS_SERVICE_HOSTNAME}:${EVENT_BUS_SERVICE_PORT}`;
+
+
 const posts = {};
 
 app.get("/posts", (req, res) => {
@@ -23,7 +28,7 @@ app.post("/posts", async (req, res) => {
     id, title
   };
 
-  await axios.post('http://localhost:4005/events', {
+  await axios.post(`${BASE_EVENT_BUS_URL}/events`, {
     type: "PostCreated",
     data: {
       id, title
