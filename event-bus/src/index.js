92fb314c10c72cbd5f1293b50d6eb57fc03e38f9
pat <dimuziop@gmail.com>
2022-02-22 07:27:37 +0100

    refactor: applying posts hostnames in event bus

--- event-bus/src/index.js
+++ event-bus/src/index.js
@@ -7,6 +7,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const POSTS_SERVICE_HOSTNAME = process.env.POSTS_SERVICE_HOSTNAME || "localhost";
+const POSTS_SERVICE_PORT = process.env.POSTS_SERVICE_PORT || "4005";
+const BASE_POSTS_URL = `http://${POSTS_SERVICE_HOSTNAME}:${POSTS_SERVICE_PORT}`;
+
+
 const events = [];
 
 
@@ -15,20 +20,20 @@ app.post("/events", (req, res) => {
 
   events.push(event);
 
-  axios.post('http://localhost:4000/events', event).catch((err) => {
+  axios.post(`${BASE_POSTS_URL}/events`, event).catch((err) => {
     console.log(err.message);
   });
-  axios.post('http://localhost:4001/events', event).catch((err) => {
-    console.log(err.message);
-  });
-  axios.post('http://localhost:4002/events', event).catch((err) => {
-    console.log(err.message);
-  });
-  res.send({ status: 'OK' });
+  // axios.post('http://localhost:4001/events', event).catch((err) => {
+  //   console.log(err.message);
+  // });
+  // axios.post('http://localhost:4002/events', event).catch((err) => {
+  //   console.log(err.message);
+  // });
+  // res.send({ status: 'OK' });
 
-  axios.post('http://localhost:4003/events', event).catch((err) => {
-    console.log(err.message);
-  });
+  // axios.post('http://localhost:4003/events', event).catch((err) => {
+  //   console.log(err.message);
+  // });
   res.send({ status: 'OK' });
 
 });
